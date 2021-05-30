10dd156358236cd90b46b71dd8136ca0350ae0a7
keryamashuna <71406785+keryamashuna@users.noreply.github.com>
2021-05-30 13:07:08 +0300

    Update flappy_bird.js

--- flappy_bird/js/flappy_bird.js
+++ flappy_bird/js/flappy_bird.js
@@ -1,7 +1,6 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
-
 var bird = new Image();
 var bg = new Image();
 var fg = new Image();
@@ -14,19 +13,23 @@ fg.src = "png/flappy_bird_fg.png";
 pipeNorth.src = "png/flappy_bird_pipeUp.png";
 pipeSouth.src = "png/flappy_bird_pipeBottom.png";
 
-
-var gap = 85;
+var gap = 120;
 var constant;
 
-var bX = 10;
-var bY = 150;
+var bX = 20;
+var bY = 200;
 
 var gravity = 1.5;
 
 var score = 0;
 
+var fly = new Audio();
+var scor = new Audio();
 
-document.addEventListener("keydown",moveUp);
+fly.src = "sounds/fly.mp3";
+scor.src = "sounds/score.mp3";
+
+document.addEventListener("click",moveUp);
 
 function moveUp(){
     bY -= 25;
@@ -39,7 +42,6 @@ pipe[0] = {
     x : cvs.width,
     y : 0
 };
-
 
 function draw(){
 
@@ -54,19 +56,18 @@ function draw(){
 
         pipe[i].x--;
 
-        if( pipe[i].x === 125 ){
+        if( pipe[i].x == 100 ){
             pipe.push({
                 x : cvs.width,
                 y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
             });
         }
 
-
         if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
             location.reload();
         }
 
-        if(pipe[i].x === 5){
+        if(pipe[i].x == 5){
             score++;
             scor.play();
         }
@@ -82,7 +83,7 @@ function draw(){
 
     ctx.fillStyle = "#000";
     ctx.font = "20px Verdana";
-    ctx.fillText("Score : "+score,10,cvs.height-20);
+    ctx.fillText("Рахунок : "+score,10,cvs.height-20);
 
     requestAnimationFrame(draw);
 
