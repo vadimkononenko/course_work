3693cf1bb04cbb6db1bc12e8e9ecfe6e7bb543da
keryamashuna <keryamashuna@gmail.com>
2021-06-03 22:56:55 +0300

    added sound effects bird animation implemented, flap wings bird rotation implemented aesthetics

--- flappy_bird_2/flappy2.js
+++ flappy_bird_2/flappy2.js
@@ -1,5 +1,6 @@
 let cvs         //  canvas
 let ctx         //  context'2d'
+let description //  game description
 let theme1      //  original theme
 let theme2      //  original them v2
 let bg          //  background
@@ -13,22 +14,35 @@ let gameOver    //  game over screen
 let map         //  map of number images
 let score       //  score counter
 let gameState   //  state of game
-let frame
+let frame       //  ms/frame = 17; dx/frame = 2; fps = 59;
+let degree      //  bird rotation degree
+const SFX_SCORE = new Audio()         //  sound for scoring
+const SFX_FLAP = new Audio()          //  sound for flying bird
+const SFX_COLLISION = new Audio()     //  sound for collision
+const SFX_FALL = new Audio()          //  sound for falling to the ground
+const SFX_SWOOSH = new Audio()        //  sound for changing game state
 
 cvs = document.getElementById('game')
 ctx = cvs.getContext('2d')
+description = document.getElementById('description')
 theme1 = new Image()
 theme1.src = 'png3/theme.png'
 theme2 = new Image()
 theme2.src = 'png3/theme-2.png'
 frame = 0;
+degree = Math.PI/180
+SFX_SCORE.src = 'audio/sfx_point.wav'
+SFX_FLAP.src = 'audio/sfx_wing.wav'
+SFX_COLLISION.src = 'audio/sfx_hit.wav'
+SFX_FALL.src = 'audio/sfx_die.wav'
+SFX_SWOOSH.src = 'audio/sfx_swooshing.wav'
+
 gameState = {
     current: 0,
     getReady: 0,
     play: 1,
     gameOver: 2
 }
-
 bg = {
     imgX: 0,
     imgY: 0,
@@ -41,9 +55,12 @@ bg = {
     dx: .2,
     render: function() {
         ctx.drawImage(theme1, this.imgX,this.imgY,this.width,this.height, this.x,this.y,this.w,this.h)
+
         ctx.drawImage(theme1, this.imgX,this.imgY,this.width,this.height, this.x + this.width,this.y,this.w,this.h)
+
         ctx.drawImage(theme1, this.imgX,this.imgY,this.width,this.height, this.x + this.width*2,this.y,this.w,this.h)
     },
+
     position: function () {
         if (gameState.current == gameState.getReady) {
             this.x = 0
@@ -53,7 +70,7 @@ bg = {
         }
     }
 }
-pipes = {
+pipes = {//object's key-value properties pinpointing its location
     top: {
         imgX: 56,
         imgY: 323,
@@ -70,6 +87,7 @@ pipes = {
     dx: 2,
     minY: -260,
     maxY: -40,
+
     pipeGenerator: [],
 
     reset: function() {
@@ -81,18 +99,17 @@ pipes = {
             let topPipe = pipe.y
             let bottomPipe = pipe.y + this.gap + this.h
 
-            ctx.drawImage(theme2, this.top.imgX, this.top.imgY, this.width, this.height, pipe.x, topPipe, this.w, this.h)
-            ctx.drawImage(theme2, this.bot.imgX, this.bot.imgY, this.width, this.height, pipe.x, bottomPipe, this.w, this.h)
+            ctx.drawImage(theme2, this.top.imgX,this.top.imgY,this.width,this.height, pipe.x,topPipe,this.w,this.h)
+            ctx.drawImage(theme2, this.bot.imgX,this.bot.imgY,this.width,this.height, pipe.x,bottomPipe,this.w,this.h)
         }
-    }
-        ,
+    },
     position: function() {
         if (gameState.current !== gameState.play) {
             return
         }
         if (gameState.current == gameState.play) {
+
             if (frame%100 == 0) {
-             //   console.log(frame)
                 this.pipeGenerator.push(
                     {
                         x: cvs.width,
@@ -100,8 +117,8 @@ pipes = {
                     }
                 )
             }
-
             for (let i = 0; i < this.pipeGenerator.length; i++) {
+
                 let pg = this.pipeGenerator[i]
                 let b = {
                     left: bird.x - bird.r,
@@ -121,23 +138,28 @@ pipes = {
                     left: pg.x,
                     right: pg.x + this.w
                 }
+
                 pg.x -= this.dx
 
                 if(pg.x < -this.w) {
                     this.pipeGenerator.shift()
                     score.current++
+                    SFX_SCORE.play()
                 }
+
                 if (b.left < p.right &&
                     b.right > p.left &&
                     b.top < p.top.bottom &&
                     b.bottom > p.top.top) {
                     gameState.current = gameState.gameOver
+                    SFX_COLLISION.play()
                 }
                 if (b.left < p.right &&
                     b.right > p.left &&
                     b.top < p.bot.bottom &&
                     b.bottom > p.bot.top) {
                     gameState.current = gameState.gameOver
+                    SFX_COLLISION.play()
                 }
             }
         }
@@ -162,12 +184,10 @@ ground = {
             this.x = 0
         }
         if (gameState.current == gameState.play) {
-            //modulus keeps x value infinitely cycling back to zero
             this.x = (this.x-this.dx) % (this.w/2)
         }
     }
 }
-//map of number images
 map = [
     num0 = {
         imgX: 496,
@@ -232,8 +252,7 @@ map = [
 ]
 score = {
     current: 0,
-    best: null, // DO THIS STRETCH GOAL
-    //values for drawing mapped numbers on canvas
+    best: null,
     x: cvs.width/2,
     y: 40,
     w: 15,
@@ -241,7 +260,6 @@ score = {
     reset: function() {
         this.current = 0
     },
-    //display the score
     render: function() {
         if (gameState.current == gameState.play ||
             gameState.current == gameState.gameOver) {
@@ -272,20 +290,30 @@ score = {
     }
 }
 bird = {
-    imgX: 276,
-    imgY: 112,
+    animation: [
+        {imgX: 276, imgY: 114},  //  position 0
+        {imgX: 276, imgY: 140},  //  position 1
+        {imgX: 276, imgY: 166},  //  position 2
+        {imgX: 276, imgY: 140}   //  position 1
+    ],
+    fr: 0,
     width: 34,
-    height: 26,
+    height: 24,
     x: 50,
     y: 160,
     w: 34,
-    h: 26,
+    h: 24,
     r: 12,
     fly: 5.25,
     gravity: .32,
     velocity: 0,
     render: function() {
-        ctx.drawImage(theme1, this.imgX,this.imgY,this.width,this.height, this.x-this.w/2,this.y-this.h/2,this.w,this.h)
+        let bird = this.animation[this.fr]
+        ctx.save()
+        ctx.translate(this.x, this.y)
+        ctx.rotate(this.rotation)
+        ctx.drawImage(theme1, bird.imgX,bird.imgY,this.width,this.height, -this.w/2,-this.h/2,this.w,this.h)
+        ctx.restore()
     },
     flap: function() {
         this.velocity = - this.fly
@@ -293,38 +321,74 @@ bird = {
     position: function() {
         if (gameState.current == gameState.getReady) {
             this.y = 160
+            this.rotation = 0 * degree
+            if (frame%20 == 0) {
+                this.fr += 1
+            }
+            if (this.fr > this.animation.length - 1) {
+                this.fr = 0
+            }
 
         } else {
+            if (frame%4 == 0) {
+                this.fr += 1
+            }
+            if (this.fr > this.animation.length - 1) {
+                this.fr = 0
+            }
 
             this.velocity += this.gravity
             this.y += this.velocity
 
+            if (this.velocity <= this.fly) {
+                this.rotation = -15 * degree
+            } else if (this.velocity >= this.fly+2) {
+                this.rotation = 70 * degree
+                this.fr = 1
+            } else {
+                this.rotation = 0
+            }
+
             if (this.y+this.h/2 >= cvs.height-ground.h) {
                 this.y = cvs.height-ground.h - this.h/2
-                gameState.current = gameState.gameOver
+                if (frame%1 == 0) {
+                    this.fr = 2
+                    this.rotation = 70 * degree
+                }
+                if (gameState.current == gameState.play) {
+                    gameState.current = gameState.gameOver
+                    SFX_FALL.play()
+                }
             }
 
             if (this.y-this.h/2 <= 0) {
                 this.y = this.r
             }
+
         }
     }
 }
 bird1 = {
-    imgX: 115,
-    imgY: 381,
-    width: 17,
+    animation: [
+        {imgX: 115, imgY: 381},  //  position 0
+        {imgX: 115, imgY: 407},  //  position 1
+        {imgX: 115, imgY: 433},  //  position 2
+        {imgX: 115, imgY: 407}   //  position 1
+    ],
+    fr: 0,
+    width: 18,
     height: 12,
     x: 50,
     y: 160,
     w: 34,
-    h: 26,
+    h: 24,
     r: 12,
     fly: 5.25,
     gravity: .32,
     velocity: 0,
     render: function() {
-        ctx.drawImage(theme2, this.imgX,this.imgY,this.width,this.height, this.x-this.w/2,this.y-this.h/2,this.w,this.h)
+        let bird = this.animation[this.fr]
+        ctx.drawImage(theme2, bird.imgX,bird.imgY,this.width,this.height, this.x-this.w/2,this.y-this.h/2,this.w,this.h)
     },
     flap: function() {
         this.velocity = - this.fly
@@ -332,13 +396,35 @@ bird1 = {
     position: function() {
         if (gameState.current == gameState.getReady) {
             this.y = 160
+            if (frame%20 == 0) {
+                this.fr += 1
+            }
+            if (this.fr > this.animation.length - 1) {
+                this.fr = 0
+            }
+
         } else {
+            if (frame%4 == 0) {
+                this.fr += 1
+            }
+            if (this.fr > this.animation.length - 1) {
+                this.fr = 0
+            }
+
             this.velocity += this.gravity
             this.y += this.velocity
+
             if (this.y+this.h/2 >= cvs.height-ground.h) {
                 this.y = cvs.height-ground.h - this.h/2
-                gameState.current = gameState.gameOver
+                if (frame%1 == 0) {
+                    this.fr = 2
+                }
+                if (gameState.current == gameState.play) {
+                    gameState.current = gameState.gameOver
+                    SFX_FALL.play()
+                }
             }
+
             if (this.y-this.h/2 <= 0) {
                 this.y = this.r
             }
@@ -346,35 +432,64 @@ bird1 = {
     }
 }
 bird2 = {
+    animation: [
+        {imgX: 87, imgY: 491},   //  position 0
+        {imgX: 115, imgY: 329},  //  position 1
+        {imgX: 115, imgY: 355},  //  position 2
+        {imgX: 115, imgY: 329}   //  position 1
+    ],
+    fr: 0,
     imgX: 87,
     imgY: 491,
-    width: 17,
+    width: 18,
     height: 12,
     x: 50,
     y: 160,
     w: 34,
-    h: 26,
+    h: 24,
     r: 12,
     fly: 5.25,
     gravity: .32,
     velocity: 0,
     render: function() {
-        ctx.drawImage(theme2, this.imgX,this.imgY,this.width,this.height, this.x-this.w/2,this.y-this.h/2,this.w,this.h)
+        let bird = this.animation[this.fr]
+        ctx.drawImage(theme2, bird.imgX,bird.imgY,this.width,this.height, this.x-this.w/2,this.y-this.h/2,this.w,this.h)
     },
-    fly: 5.8,
     flap: function() {
         this.velocity = - this.fly
     },
     position: function() {
         if (gameState.current == gameState.getReady) {
             this.y = 160
+            if (frame%20 == 0) {
+                this.fr += 1
+            }
+            if (this.fr > this.animation.length - 1) {
+                this.fr = 0
+            }
+
         } else {
+            if (frame%4 == 0) {
+                this.fr += 1
+            }
+            if (this.fr > this.animation.length - 1) {
+                this.fr = 0
+            }
+
             this.velocity += this.gravity
             this.y += this.velocity
+
             if (this.y+this.h/2 >= cvs.height-ground.h) {
                 this.y = cvs.height-ground.h - this.h/2
-                gameState.current = gameState.gameOver
+                if (frame%1 == 0) {
+                    this.fr = 2
+                }
+                if (gameState.current == gameState.play) {
+                    gameState.current = gameState.gameOver
+                    SFX_FALL.play()
+                }
             }
+
             if (this.y-this.h/2 <= 0) {
                 this.y = this.r
             }
@@ -408,9 +523,11 @@ gameOver = {
     render: function() {
         if (gameState.current == gameState.gameOver) {
             ctx.drawImage(theme1, this.imgX,this.imgY,this.width,this.height, this.x,this.y,this.w,this.h)
+            description.style.visibility = "visible"
         }
     }
 }
+
 let draw = () => {
     ctx.fillStyle = '#00bbc4'
     ctx.fillRect(0,0, cvs.width,cvs.height)
@@ -432,11 +549,9 @@ let loop = () => {
     draw()
     update()
     frame++
-   // requestAnimationFrame(loop)
 }
 loop()
 setInterval(loop, 17)
-
 
 cvs.addEventListener('click', () => {
     if (gameState.current == gameState.getReady) {
@@ -444,9 +559,14 @@ cvs.addEventListener('click', () => {
     }
     if (gameState.current == gameState.play) {
         bird.flap()
+        SFX_FLAP.play()
+        description.style.visibility = "hidden"
     }
     if (gameState.current == gameState.gameOver) {
+        pipes.reset()
+        score.reset()
         gameState.current = gameState.getReady
+        SFX_SWOOSH.play()
     }
 })
 document.body.addEventListener('keydown', (e) => {
@@ -454,14 +574,17 @@ document.body.addEventListener('keydown', (e) => {
         if (gameState.current == gameState.getReady) {
             gameState.current = gameState.play
         }
+        //if play state >> bird keeps flying
         if (gameState.current == gameState.play) {
             bird.flap()
+            SFX_FLAP.play()
+            description.style.visibility = "hidden"
         }
         if (gameState.current == gameState.gameOver) {
             pipes.reset()
             score.reset()
+            SFX_SWOOSH.play()
             gameState.current = gameState.getReady
         }
     }
 })
-
